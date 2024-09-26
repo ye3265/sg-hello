199d44e6a249f7ba56ca222a598ea62de01e6402
kimyg <ye3265@gmail.com>
2024-09-26 14:06:53 +0900

    second commit

--- src/main/java/com/ye3265/controller/HelloController.java
+++ src/main/java/com/ye3265/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 	@GetMapping("/hello")
 	public String helloPage(Model model) {
-		model.addAttribute("greeting", "Hi!");
+		model.addAttribute("greeting", "Hahaha");
 		return "hello";
 		
 	}
